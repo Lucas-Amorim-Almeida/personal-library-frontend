a1300093059a62d65572f1d90a505f1c59c6453e
Lucas-Amorim-Almeida <si.lucas.almeida@gmail.com>
2025-03-08 17:02:50 -0300

    feat: adiciona prototipos da UserPage e AddBook
    - Adiciona userPage.html e addBook.html;
    - Adiciona script de redirecionamento em userPage.html para addBook.html;
    - Adiciona stylesheet parcial de userPage.html;
    - Refatora outras stylesheets.

---
+++ app/js/pages/userPage/handleClickOptionsButtons.js
@@ -0,0 +1,9 @@
+const addBook = document.querySelector("#add-book");
+addBook.addEventListener("click", () => {
+  window.location.href = "./addBook.html";
+});
+
+const createCollection = document.querySelector("#create-collection");
+createCollection.addEventListener("click", () => {
+  window.location.href = "./createCollection.html";
+});
